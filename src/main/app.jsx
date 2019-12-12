1b989ef2b131b97884bc64b364ec4738c9068809
anderson silva <anderson_silvaribeiro@hotmail.com>
2019-12-12 20:02:37 -0300

    implemntação footer

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className="wrapper">
         <Header />
-        <SideBar/>
+        <SideBar />
+        <div className="content-wrapper">
+            <h1>Conteúdo</h1>
+        </div>
+        <Footer />
     </div>
 )
 
