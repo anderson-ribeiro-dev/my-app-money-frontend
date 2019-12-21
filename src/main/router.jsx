b619ad1c654030f1286de528d231d936ec573dbf
anderson silva <anderson_silvaribeiro@hotmail.com>
2019-12-21 19:56:16 -0300

    criando componente de class BillingCycle

--- src/main/router.jsx
+++ src/main/router.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
 import Dashboard from '../dashboard/dashboard'
+// import Dashboard2 from '../dashboard2/dashboard2'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
+        {/* <Route path='/' component={Dashboard} /> */}
         <Route path='/' component={Dashboard} />
         <Route path='/billingCycle' component={BillingCycle} />
         <Redirect from='*'  to='/' />
