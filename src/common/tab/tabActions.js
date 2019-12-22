fe21211d71c9a96036f42dfae9af5972ee1d23dd
anderson silva <anderson_silvaribeiro@hotmail.com>
2019-12-22 18:32:57 -0300

    criação tabActions/ TabReducers - tab-selected

---
+++ src/common/tab/tabActions.js
@@ -0,0 +1,9 @@
+export function selectTab(tabId) {
+    console.log(tabId)
+
+    //retorna um action creator
+    return {
+        type: 'TAB_SELECTED',
+        payload: tabId
+    }
+}
---
+++ src/common/tab/tabReducer.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = { selected: '' }
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'TAB_SELECTED':
+        default:
+            return state
+    }
+}
