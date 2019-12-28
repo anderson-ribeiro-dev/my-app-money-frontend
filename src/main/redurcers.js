11da0ca22128ea004dbcdf77965eacba48a1c219
anderson silva <anderson_silvaribeiro@hotmail.com>
2019-12-28 18:22:03 -0300

    criando getList action e reducer billingCycle

--- src/main/redurcers.js
+++ src/main/redurcers.js
@@ -3,10 +3,12 @@ import {  combineReducers }  from 'redux'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/BillingCycleReducer'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer
 })
 
 export default rootReducer
