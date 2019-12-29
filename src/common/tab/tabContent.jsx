a4a975e94c46be517ffb13c20ab7a0405d3ad517
anderson silva <anderson_silvaribeiro@hotmail.com>
2019-12-29 16:57:16 -0300

    criação componente BillingCycleList

--- src/common/tab/tabContent.jsx
+++ src/common/tab/tabContent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { bindActionCreators } from 'react'
+import { bindActionCreators  } from 'redux'
 import { connect } from 'react-redux'
 
 import If from '../operador/if'
@@ -9,7 +9,7 @@ import If from '../operador/if'
 class TabContent extends Component {
     render(){
         const selected = this.props.tab.selected === this.props.id
-        const visible = this.props.tab.selected[this.props.id]
+        const visible = this.props.tab.visible[this.props.id]
         return (
             <If test={visible}>
                 <div id={this.props.id}
